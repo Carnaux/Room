b65985b229dc65faec12ff677e1cc147224e4b92
Daniel Fernandes Gonçalves de Oliveira <danielfeenandes@gmail.com>
2019-08-07 15:48:47 -0300

    Update editor.js

--- newOne/editor/editor.js
+++ newOne/editor/editor.js
@@ -1,3 +1,8 @@
+// ************************************
+// Created by Daniel Fernandes - 2019
+// carnaux.github.io
+// ************************************
+
 var scene, camera, renderer, orbit, control, gridHelper;
 
 let interestDatabase = ["food", "books"];
